341c15468f63296fa9d96aaf8b00eb483eefe51d
rayan nahi <rayan.nahi99@gmail.com>
2021-09-16 23:44:30 +0200

    ajout protection contre les injections

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -15,8 +15,8 @@ exports.createPost = (req, res , next) => {
     }
     const post_text = sanitizehtml(req.body.post_text);
     const nickname = decodedToken.nickname;
-    var sql = `INSERT INTO posts (nickname, imageUrl, post_text, masked,date) VALUES ('${nickname}','${imageUrl}',"${post_text}",0,'${req.body.date}')`; // configuration de l'article
-    db.query(sql, function (err, result) {
+; // configuration de l'article
+    db.query(`INSERT INTO posts (nickname, imageUrl, post_text, masked,date) VALUES ('${nickname}','${imageUrl}',?,0,'${req.body.date}')`, post_text, function (err, result) {
           if (err) throw err;
           db.query(`SELECT * FROM posts ORDER BY id DESC`, function (err, result,fields) { // Renvoi de la liste des articles pour l'affichage dans le front-end
             if (err){
@@ -97,7 +97,7 @@ exports.updatePost = (req,res,next) => {
         })
       }
     }
-    db.query(`UPDATE posts SET post_text = "${sanitizehtml(req.body.post_text)}" WHERE id = '${req.params.id}'`, function (err, result,fields) { // Mise à jour du texte de l'article
+    db.query(`UPDATE posts SET post_text = ? WHERE id = '${req.params.id}'`,sanitizehtml(req.body.post_text), function (err, result,fields) { // Mise à jour du texte de l'article
       if (err){
          return res.status(403).json({error : err});
       };
